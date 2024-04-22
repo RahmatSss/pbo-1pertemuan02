540fbed8541e6434b5301fbed689336a09d25996
RahmatSss <rahmatsantoso5514@gmail.com>
2024-04-22 11:49:13 +0800

    InputOutput.java

---
+++ model/inputoutput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class inputoutput{
+    public static void main(string[] args){
+        string nama = "Rahmat";
+        Scanner scanner = new Scanner(System.in);
+    }
+    int hitungNilai(){
+        return 0;
+    }
+}
